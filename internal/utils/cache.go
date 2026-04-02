d3737230232b956fed36c2842b20f888ec3e500a
Tosu <lifoxff0825@gmail.com>
2026-04-03 01:00:20 +0530

    ~

--- internal/utils/cache.go
+++ internal/utils/cache.go
@@ -12,6 +12,8 @@ import (
 	"time"
 )
 
+const NoExpiry = time.Duration(-1)
+
 type CacheItem[V any] struct {
 	Value      V
 	Expiration time.Time
@@ -75,4 +77,24 @@ func (c *Cache[K, V]) Delete(key K) {
 	c.mu.Lock()
 	delete(c.items, key)
 	c.mu.Unlock()
+}
+
+func (c *Cache[K, V]) LoadAndDelete(key K) (V, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	item, ok := c.items[key]
+	if !ok {
+		var zero V
+		return zero, false
+	}
+
+	delete(c.items, key)
+
+	if item.Expired() {
+		var zero V
+		return zero, false
+	}
+
+	return item.Value, true
 }
